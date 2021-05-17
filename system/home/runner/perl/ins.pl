8fa8fdad6d8c293029df7f0eac32557d58417e8e
kaigonzalez <gkai70264@gmail.com>
2021-05-17 11:13:59 -0400

    add a chroot terminal

---
+++ system/home/runner/perl/ins.pl
@@ -0,0 +1,34 @@
+use strict;
+
+
+use Curses;
+initscr;
+
+sub printf
+{
+    my $newwstr  = join(' ', @_);
+    Curses::printw($newwstr + "\n");
+};
+while (1) {
+
+    Curses::printw("chroot\$runner-# ");
+
+    my $str = getstring();
+    my @args = split(" ", $str);
+    my $command = $args[0];
+
+    if ($command == "dir")
+    {
+        printw("you are inside chroot/home/runner/\n");
+    }
+    elsif ($command == "exit") {
+        endwin();
+    }
+    else
+    {
+        printw("bash: invalid command\n");
+    }
+}
+
+getchar;
+endwin
