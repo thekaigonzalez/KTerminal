acae103ee9ed92db6c38c04a548e7b4cdae109f7
kaigonzalez <gkai70264@gmail.com>
2021-05-17 09:54:48 -0400

    Update the app handler. Didn't work at first.

--- usr/bin/apps.py
+++ usr/bin/apps.py
@@ -3,9 +3,5 @@ import sys
 
 
 def main(s, a, c, o):
-    from subprocess import Popen, PIPE
 
-    p = Popen(['python3', 'system/home/apps/' + a[1] + ".py"], stdout=PIPE, stderr=PIPE, stdin=PIPE)
-
-    output = p.stdout.read()
-    p.stdin.write(output)
+    os.system('python3 system/home/apps/' + a[0] + ".py")
