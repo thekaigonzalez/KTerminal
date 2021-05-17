86226c8647e89a3a7c95189b249ed61c808e7fa3
kaigonzalez <gkai70264@gmail.com>
2021-05-17 09:53:04 -0400

    Update vim.py to not corrupt your terminal

--- system/home/apps/vim.py
+++ system/home/apps/vim.py
@@ -1,20 +1,22 @@
 import curses
 # family dollar vim.. gets the job done.
+try:
+    import curses.textpad
 
-import curses.textpad
+    stdscr = curses.initscr()
 
-stdscr = curses.initscr()
+    curses.noecho()
 
-curses.noecho()
+    curses.cbreak()
 
-curses.cbreak()
+    stdscr.keypad(True)
+    stdscr.clear()
+    stdscr.refresh()
+    win = curses.newwin(5, 60, 5, 10)
+    tb = curses.textpad.Textbox(win)
 
-stdscr.keypad(True)
-stdscr.clear()
-stdscr.refresh()
-win = curses.newwin(5, 60, 5, 10)
-tb = curses.textpad.Textbox(win)
-
-text = tb.edit()
-curses.beep()
-win.addstr(5,2,text.encode('utf_8'))
+    text = tb.edit()
+    curses.beep()
+    win.addstr(5,2,text.encode('utf_8'))
+except KeyboardInterrupt:
+    curses.endwin() # end the window and return to root.
