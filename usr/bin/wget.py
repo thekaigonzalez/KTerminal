dce4deead891ecc68236d9b4f49f9ce4d6755ace
kaigonzalez <gkai70264@gmail.com>
2021-05-18 10:25:57 -0400

    Fix `wget`

--- usr/bin/wget.py
+++ usr/bin/wget.py
@@ -4,19 +4,19 @@ def main(s,a,c,o):
     if c == 0:
         s.addstr("missing (url).\n")
     elif c == 1:
-        s.addstr("getting " + a[0])
-        s.addstr("download " + a[0])
+        s.addstr("getting " + a[0] + "\n")
+        s.addstr("download " + a[0] + "\n")
         re = requests.request('GET', a[0])
-        if (re.status_code == 400):
+        if (re.status_code == 404):
             s.addstr("download failed: server responded with code 400.\n")
         elif re.status_code == 200:
             s.addstr("download success: server responded with code 200.\n")
-            stri = a[0]
-            bb = str(stri)
 
-            fname = bb[bb.find('/')+1:len(bb)]
+            bb = re.url
+
+            fname = bb[bb.rfind('/')+1:len(bb)]
 
             s.addstr("creating file " + fname + "\n")
-            y = open(fname , 'w')
+            y = open(o[4] + "/" + fname , 'w')
             y.write(re.text)
             y.close()
