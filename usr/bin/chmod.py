c41e405479f02b39c0c71faa4cfc6e2b5b824198
kaigonzalez <gkai70264@gmail.com>
2021-05-24 11:58:34 -0400

    Add a driver checker "Checker Mod " in early access

---
+++ usr/bin/chmod.py
@@ -0,0 +1,20 @@
+import pathlib
+
+ROOT = False
+
+
+def main(s,a,c,o):
+
+    if len(o) == 6:
+        if len(a) == 0:
+            s.addstr("CHMOD Utility\n\tCheck system drivers and ensure everything is up to date.\n\nARGUMENTS\n\t[-f "
+                     "| -c | -k]\n")
+        elif len(a) == 2:
+            if a[0] == "-f":
+                driver = a[1]
+                if pathlib.Path("drivers/" + driver).exists():
+                    s.addstr("chmod: driver exists in drivers folder and is not corrupted\n")
+                else:
+                    s.addstr("chmod: driver does not exist.\n")
+    else:
+        s.addstr("I do not have full access to your files! please run chmod with the command:\nrooty chmod " + ' '.join(a) + "\n")
