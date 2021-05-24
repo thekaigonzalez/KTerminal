8724517db85cc5246f0e321f6d6a680cd5cf3f30
kaigonzalez <gkai70264@gmail.com>
2021-05-24 12:54:58 -0400

    Update MVEF

--- usr/bin/mvef.py
+++ usr/bin/mvef.py
@@ -3,7 +3,10 @@ import pathlib
 
 
 def main(s: curses.window,a,c,o):
-    if pathlib.Path(a[0]).exists():
-        s.addstr("yes")
+    if len(o) == 6:
+        if pathlib.Path(a[0]).exists():
+            s.addstr("yes\n")
+        else:
+            s.addstr("no.\n")
     else:
-        s.addstr("no.")
+        s.addstr("Run mvef from Root to prevent problems. `sudo mvef <f>`\n")
