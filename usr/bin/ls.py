626fb25554861c21360fc7b7bde26176aa181bce
kaigonzalez <gkai70264@gmail.com>
2021-05-27 19:14:51 -0400

    update ls.py

--- usr/bin/ls.py
+++ usr/bin/ls.py
@@ -4,8 +4,8 @@ import os
 def main(s,a,c,o):
 
     if c == 0:
-        s.addstr("Description: Lists Subdirectories in a given directory\nUsage: ls [-opts] [dir] or ls "
-                      "[dir]\n")
+        for i in os.listdir(o[3]):
+        	s.addstr(i + "\n")
     elif c == 1:
 
         for file in os.listdir(a[0]):
