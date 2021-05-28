91d4fa4344d3b60490545d94898e76402f4f39cf
kaigonzalez <gkai70264@gmail.com>
2021-05-28 03:08:04 -0400

    Testing some primitive types

--- terminal/dtypes/__init__.py
+++ terminal/dtypes/__init__.py
@@ -8,3 +8,13 @@ def checknumber(argState: list, pos: int):
 
 def checkClass(argState: list, pos: int, classf):
     return classf(argState[pos])
+
+# checks an argument and potentially stores it into an array parsing it using [].
+def checkarray(argState: list, IsNearestInstance: bool, pos: int = 0):
+    if IsNearestInstance == True:
+        s = ' '.join(argState)
+        array_cn = s[s.find("[")+1:s.rfind("]")]
+        narr = []
+        for i in array_cn.split(","):
+            narr.append(i)
+        return narr
