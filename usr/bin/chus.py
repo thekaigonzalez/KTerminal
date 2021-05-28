328d9b69cd29555bb90e506a2a8e1b4aa611de99
kaigonzalez <gkai70264@gmail.com>
2021-05-28 11:02:43 -0400

    Add usernames!

--- usr/bin/chus.py
+++ usr/bin/chus.py
@@ -7,6 +7,6 @@ printc = IO.printw
 
 def main(s: curses.window, a: list, c: int, o: list):
     """ basic utility to change the user name, nothing too big """
-    a = open('usr/user.txt', 'w')
-    a.write(' '.join(a))
-    a.close()
+    b = open('usr/user.txt', 'w')
+    b.write(' '.join(a))
+    b.close()
