c8324f226c908c69d95fc10c6c1e790ed08e26c2
kaigonzalez <gkai70264@gmail.com>
2021-05-28 15:36:02 -0400

    Add Documentation

--- terminal/io/__init__.py
+++ terminal/io/__init__.py
@@ -1,5 +1,11 @@
 import curses
-
+stdscr = curses.initscr()
 def printw(s: str):
+    """
+    Creates a screen and prints to the screen, simulating a stdscr print.
+    You can also use the global variable "stdscr".
+    :param s:
+    :return:
+    """
     wnd = curses.initscr()
     wnd.addstr(s + "\n")
