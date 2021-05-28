6460cf9a0ef4c62ed0942e7ef22e073f9cd66007
kaigonzalez <gkai70264@gmail.com>
2021-05-28 02:32:30 -0400

    Update `ls`

--- usr/bin/ls.py
+++ usr/bin/ls.py
@@ -5,7 +5,8 @@ def main(s,a,c,o):
 
     if c == 0:
         for i in os.listdir(o[3]):
-        	s.addstr(i + "\n")
+            s.addstr(i + " ")
+        s.addstr("\n")
     elif c == 1:
 
         for file in os.listdir(a[0]):
