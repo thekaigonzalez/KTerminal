3f603906e9602a7efd3e08a9c7f4a8e55a8baef2
kaigonzalez <gkai70264@gmail.com>
2021-05-29 12:13:22 -0400

    Add check encryption

--- usr/bin/chencr.py
+++ usr/bin/chencr.py
@@ -3,15 +3,19 @@ import os
 
 import pathlib
 
+import terminal.dir
 import terminal.io as IO
-
+import terminal.parser
 def printf(vs):
     IO.printw(vs)
 
 
 def main(s: curses.window, a: list, c: int, o: list):
-    """ Checks the level of encryption for a directory """
-    if (pathlib.Path(a[0] + "/IS_ENCRYPTED_SYSTEM").exists()):
-        printf("chencr: path exists; is encrypted")
+    """ Checks the level of encryption for a directory using the Terminal Encryption API
+    (USE QUOTATION MARKS FOR DIRECTORIES WITH SPACES!)
+    """
+    userdir = terminal.dir.Directory(terminal.parser.checkquotedstring(0, a))
+    if userdir.Encrypted():
+        printf("Directory encrypted: true")
     else:
-        printf("path does not exist; not system directory")
+        printf("directory encrypted: false")
