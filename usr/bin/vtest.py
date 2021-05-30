5b237cbb6e17927978e0a6ab811164e43e60c29b
kaigonzalez <gkai70264@gmail.com>
2021-05-29 20:30:24 -0400

    break my back for bootleg argparse **WITH VARIABLES**

---
+++ usr/bin/vtest.py
@@ -0,0 +1,16 @@
+import curses
+import os
+import terminal.io as IO
+import terminal.argsetup as AS
+def printf(vs):
+    IO.printw(vs)
+def HelpCommand(a, o):
+    o.sendhelp()
+
+def main(s: curses.window, a: list, c: int, o: list):
+    """ Tests variables... Yes... """
+    parser = AS.ArgumentPasser(a)
+    parser.extract(0)
+    parser.bind
+    printf(parser.parseconfig_getpos("VALUE", 0)) # Should return the first variable...
+    parser.bindonce("h", HelpCommand)
